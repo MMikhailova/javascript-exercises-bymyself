a3651f78ca0f9b733ed2b191d6eb1cce67ad4c34
Maria <naplesaries2014@gmail.com>
2022-05-27 14:29:35 +0200

    tried onclick

--- 4-empty/exercise/script.js
+++ 4-empty/exercise/script.js
@@ -1,4 +1,9 @@
+/*var myButton = document.querySelector('button');
+myButton.onclick = function checkJavascript() {
+  document.getElementById("intro").style.display = "none";
+}*/
+
 function checkJavascript() {
   document.getElementById("intro").style.display = "none";
-  document.getElementById("questions").style.display = "block";
 }
+  

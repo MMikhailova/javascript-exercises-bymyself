e781a9ab0928caf98f70949e07c64a028ffd9c23
Maria <naplesaries2014@gmail.com>
2022-05-28 16:34:48 +0200

    added js

--- 4-empty/exercise/script.js
+++ 4-empty/exercise/script.js
@@ -3,7 +3,20 @@ myButton.onclick = function checkJavascript() {
   document.getElementById("intro").style.display = "none";
 }*/
 
-function checkJavascript() {
-  document.getElementById("intro").style.display = "none";
-}
+  // anotherFunction() only executed if no text given
+  // its result becomes the value of text
+'use strict'
+function checkName() {
+  const name = document.querySelector("#input").value;
+  debugger;
+return name === ''? alert('entre your name'):changeForm(name)
+  }
   
+
+function changeForm(name) {
+
+  let form = document.getElementById("questions");
+  document.querySelector("h1").innerText = 'Hi, '+name;
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("questions").style.display = "block";
+}

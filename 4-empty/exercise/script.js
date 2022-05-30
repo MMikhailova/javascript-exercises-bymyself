7188b33da919a490c77f5fd6b6c34ea04a52bbfa
Maria <naplesaries2014@gmail.com>
2022-05-30 15:13:49 +0200

    changed 4-empty/exercise/script.js

--- 4-empty/exercise/script.js
+++ 4-empty/exercise/script.js
@@ -6,17 +6,42 @@ myButton.onclick = function checkJavascript() {
   // anotherFunction() only executed if no text given
   // its result becomes the value of text
 'use strict'
-function checkName() {
-  const name = document.querySelector("#input").value;
+const checkName = () => {
   debugger;
-return name === ''? alert('entre your name'):changeForm(name)
+  const name = document.querySelector("#input").value;
+  return name === ''? alert('entre your name'):changeForm(name)
   }
-  
 
-function changeForm(name) {
 
-  let form = document.getElementById("questions");
+const changeForm=(name)=> {
+
+  let form = document.getElementById("form");
   document.querySelector("h1").innerText = 'Hi, '+name;
   document.getElementById("intro").style.display = "none";
-  document.getElementById("questions").style.display = "block";
+  document.getElementById("form").style.display = "block";
 }
+let i=0
+let changeContent = () => {
+  const questionList = [
+    "Finish this sequence: 'un', 'deux', 'trois'...",
+    "Volvo",
+    "BMW",
+  ];
+  const answers = ['un', 'deux', 'trois','cinq','six','huit'
+  ];
+  if (i == 0) {
+    var header = document.querySelector("h1");
+    header.remove();
+    document.getElementById("question").innerText = questionList[i];
+    document.getElementById("option1").innerText = answers[i];
+    document.getElementById("option2").innerText = answers[i++];
+    document.getElementById("option3").innerText = answers[i+2];
+    i++;
+    
+  } else {
+    document.getElementById("question").innerText = questionList[i];
+    i++;
+  }  
+  }
+
+

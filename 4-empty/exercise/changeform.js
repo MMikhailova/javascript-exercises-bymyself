6495e8b7966bb62eea8f11818007ba7000880853
Maria <naplesaries2014@gmail.com>
2022-06-14 13:52:59 +0200

    code for changing the intro form

---
+++ 4-empty/exercise/changeform.js
@@ -0,0 +1,30 @@
+
+const changeForm = () => {
+    debugger;
+  let form = document.getElementById("form");
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("form").style.display = "block";
+};
+
+document.getElementById("start").addEventListener("click", changeForm);
+/**let i = 0;
+let changeContent = () => {
+  const questionList = [
+    "Finish this sequence: 'un', 'deux', 'trois'...",
+    "Volvo",
+    "BMW",
+  ];
+  const answers = ["un", "deux", "trois", "cinq", "six", "huit"];
+  if (i == 0) {
+    var header = document.querySelector("h1");
+    header.remove();
+    document.getElementById("question").innerText = questionList[i];
+    document.getElementById("option1").innerText = answers[i];
+    document.getElementById("option2").innerText = answers[i++];
+    document.getElementById("option3").innerText = answers[i + 2];
+    i++;
+  } else {
+    document.getElementById("question").innerText = questionList[i];
+    i++;
+  }
+};***/
